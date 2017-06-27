84a1dca5e068efc6641821ffd2037d075b6ddff0
Tyler Van Gorder <tyler.vangorder@build.com>
2017-06-26 17:19:20 -0700

    Adding tests that work, along with the one that does not.

--- src/test/java/com/example/demo/DemoApplicationTestsWithListner.java
+++ src/test/java/com/example/demo/DemoApplicationTestsWithListner.java
@@ -8,17 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.MockitoTestExecutionListener;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringRunner;
 
-/**
- * This test is broken because it extends AbstractTransactionalJUnit4SpringContextTests
- * @author tyler.vangorder
- *
- */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.NONE, classes= {MyConfiguration.class})
-public class DemoApplicationTests extends AbstractTransactionalJUnit4SpringContextTests{
+@TestExecutionListeners(MockitoTestExecutionListener.class)
+public class DemoApplicationTestsWithListner extends AbstractTransactionalJUnit4SpringContextTests{
 
 	@MockBean
 	Bar bar;
